875db0990458fc9f487b4bca46d9da11171e5be8
bluehawk27 <avillacorta27@gmail.com>
2017-11-10 16:30:13 -0800

    initial commit

---
+++ todo/pkg/service/service.go
@@ -0,0 +1,7 @@
+package service
+
+// TodoService describes the service.
+type TodoService interface {
+	// Add your methods here
+	// e.x: Foo(ctx context.Context,s string)(rs string, err error)
+}
